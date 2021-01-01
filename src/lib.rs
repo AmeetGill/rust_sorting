61baed1dc773c60aed11ed6db15752e3c46f9098
amesingh <ameetpal.singh@oracle.com>
2021-01-01 10:51:29 +0000

    added merge sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod insertion;
 pub mod quick;
+pub mod merge;
 
 #[cfg(test)]
 mod test {
@@ -78,5 +79,64 @@ mod test {
         assert_eq!(array_to_sort,copy_array_to_sort);
 
     }
+
+   #[test]
+    fn check_merge_sort_unique_values() {
+
+        let mut array_to_sort = vec![3,6,4,31,12,34,5,1,22,55];
+
+        let mut copy_array_to_sort = array_to_sort.clone();
+
+        copy_array_to_sort.sort();
+
+        merge::sort(&mut array_to_sort);
+
+        assert_eq!(array_to_sort,copy_array_to_sort);
+
+    }
+
+
+   #[test]
+   fn check_merge_sort_repeating_values() {
+
+       let mut array_to_sort = vec![3,6,12,4,31,1,3,5,12,34,5,1,22,55];
+       let mut copy_array_to_sort = array_to_sort.clone();
+
+       copy_array_to_sort.sort();
+       merge::sort(&mut array_to_sort);
+
+       assert_eq!(array_to_sort,copy_array_to_sort);
+
+   }
+
+   #[test]
+   fn check_merge_sort_inplace_unique_values() {
+
+        let mut array_to_sort = vec![3,6,4,31,12,34,5,1,22,55];
+
+        let mut copy_array_to_sort = array_to_sort.clone();
+
+        copy_array_to_sort.sort();
+
+        merge::sort_inplace(&mut array_to_sort);
+
+        assert_eq!(array_to_sort,copy_array_to_sort);
+
+    }
+
+
+   #[test]
+   fn check_merge_sort_inplace_repeating_values() {
+
+       let mut array_to_sort = vec![3,6,12,4,31,1,3,5,12,34,5,1,22,55];
+       let mut copy_array_to_sort = array_to_sort.clone();
+
+       copy_array_to_sort.sort();
+       merge::sort_inplace(&mut array_to_sort);
+
+       assert_eq!(array_to_sort,copy_array_to_sort);
+
+   }
+
 
 }
--- src/merge.rs
+++ src/merge.rs
@@ -1,28 +1,36 @@
-fn sort(input: &mut Vec<i32>){
-   sort_util(input, 0, input.len()); 
+pub fn sort(input: &mut Vec<i32>) {
+   sort_util(input, 0, input.len()-1,false); 
 }
 
-fn sort_util(input: &mut Vec<i32>, start: usize, end: usize) {
-    if start == end {
-        return ();
+pub fn sort_inplace(input: &mut Vec<i32>) {
+    sort_util(input, 0, input.len()-1,true);
+}
+
+fn sort_util(input: &mut Vec<i32>, start: usize, end: usize, inplace: bool) {
+    if start < end {
+
+        let mid = (start + end)/2;
+
+        sort_util(input, start, mid, inplace);
+        sort_util(input, mid + 1, end, inplace);
+        if inplace {
+            merge_inplace(input, start, mid, end);
+        } else {
+            merge(input, start, mid, end);
+        }
+
     }
-
-    let mid = (start + end)/2;
-
-    sort_util(input, start, mid);
-    sort_util(input, mid + 1, end);
-    merge(input, start, mid, end);
 }
 
 fn merge(input: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
     
     let mut counter_1 = start;
     let mut counter_2 = mid + 1;
-    let mut i = 0;
-    let arr: [i32, end-start + 1] = {};
+    let mut i: usize = 0; 
+    let mut arr = vec![0; end - start + 1];
 
     while counter_1 <= mid && counter_2 <= end {
-        if input[counter_1] >= input[counter_2] {
+        if input[counter_1] <= input[counter_2]{
             arr[i] = input[counter_1];
             counter_1 += 1;
         }else {
@@ -32,7 +40,6 @@ fn merge(input: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
         i += 1;
     }
 
-
     while counter_1 <= mid {
         arr[i] = input[counter_1];
         counter_1 += 1;
@@ -41,12 +48,51 @@ fn merge(input: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
 
     while counter_2 <= end {
         arr[i] = input[counter_2];
-        counter_2 += 2;
+        counter_2 += 1;
         i += 1;
     }
+
     i = start;
+    let mut j = 0;
     while i <= end {
-        input[i] = arr[i]
+        input[i] = arr[j];
         i += 1;
+        j += 1;
     }
+}
+
+
+fn merge_inplace(input: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
+
+    let mut counter_1 = start;
+    let mut mid_1 = mid;
+    let mut counter_2 = mid + 1;
+   // [11,12,20,3,9,31]
+   // [11,11,12,20,9,31]
+
+    while counter_1 <= mid_1 && counter_2 <= end {
+        if input[counter_1] <= input[counter_2] {
+            counter_1 += 1;
+        } else {
+            let temp = input[counter_2];
+            right_shift_by_1(input, counter_1, counter_2);
+            mid_1 += 1;
+            input[counter_1] = temp;
+            counter_2 += 1;
+            counter_1 += 1;
+        }
+    }
+}
+
+fn right_shift_by_1(input: &mut Vec<i32>, start: usize, end: usize) {
+    if start > end {
+        panic!("cannot right shift this array");
+    }
+
+    let mut i = end;
+    while i > start { 
+        input[i] = input[i-1];
+        i -= 1;
+    }
+    
 }
